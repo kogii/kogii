78ff4702a9f9791b3bc7954070cc8b962cadb73d
TanakaDaisuke <tanakadaisuke@PC-6080.local>
2019-06-29 11:26:50 +0900

    各曜日にリンク追加&コメント付けた

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -1,6 +1,6 @@
 class LecturesController < ApplicationController
     def show
-        @lecture = Timetable.find_by(:id => params[:id]) #nameをもとにデータベースから
+        @lecture = Timetable.find_by(:id => params[:id]) #idをもとにデータベースから講義情報を持ってくる
 
     end
 end
